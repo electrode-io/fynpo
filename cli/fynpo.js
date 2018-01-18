0a6a8d3baf4159c8ef4f97c632cef00e627ed02c
Joel Chen <xchen@walmartlabs.com>
2018-01-18 11:41:34 -0800

    prepare for publish

--- cli/fynpo.js
+++ cli/fynpo.js
@@ -2,23 +2,38 @@
 "use strict";
 
 const Path = require("path");
+const NixClap = require("nix-clap");
 const Bootstrap = require("../lib/bootstrap");
+const Prepare = require("../lib/prepare");
 const makePkgDeps = require("../lib/make-pkg-deps");
 const readPackages = require("../lib/read-packages");
-const NixClap = require("nix-clap");
+const logger = require("../lib/logger");
+
+const makeBootstrap = parsed => {
+  const cwd = parsed.opts.cwd || process.cwd();
+  return new Bootstrap(makePkgDeps(readPackages(cwd), parsed.opts.ignore || []));
+};
 
 const execBootstrap = parsed => {
-  const cwd = parsed.opts.cwd || process.cwd();
-  const bootstrap = new Bootstrap(
-    makePkgDeps(readPackages(cwd), parsed.opts.ignore || [])
-  ).bootstrap();
+  const bootstrap = makeBootstrap(parsed);
+  return bootstrap
+    .exec()
+    .then(() => {
+      process.exit(bootstrap.failed);
+    })
+    .catch(err => {
+      logger.error(err);
+      process.exit(1);
+    });
 };
 
 const execLocal = parsed => {
+  return makeBootstrap(parsed).updateToLocal();
+};
+
+const execPrepare = parsed => {
   const cwd = parsed.opts.cwd || process.cwd();
-  const bootstrap = new Bootstrap(
-    makePkgDeps(readPackages(cwd), parsed.opts.ignore || [])
-  ).updateToLocal();
+  return new Prepare(cwd, makePkgDeps(readPackages(cwd), parsed.opts.ignore) || []).exec();
 };
 
 const nixClap = new NixClap({
@@ -54,8 +69,13 @@ const nixClap = new NixClap({
       alias: "l",
       desc: "update packages dependencies to point to local",
       exec: execLocal
+    },
+    prepare: {
+      alias: "p",
+      desc: "Prepare packages versions for publish",
+      exec: execPrepare
     }
   }
 );
 
-const parsed = nixClap.parse();
+nixClap.parseAsync();
