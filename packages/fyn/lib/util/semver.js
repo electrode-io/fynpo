881ee231b0c90ba8a6f5347c83bc770b312623b6
Joel Chen <joel123@gmail.com>
2018-01-26 00:46:07 -0800

    handle semver better

--- packages/fyn/lib/util/semver.js
+++ packages/fyn/lib/util/semver.js
@@ -12,21 +12,33 @@ const Semver = require("semver");
 
 /* eslint-disable max-statements */
 
+function split(v, sep, index = 0) {
+  const x = v.indexOf(sep, index);
+  if (x >= index) {
+    return [v.substr(0, x), v.substr(x + 1)];
+  }
+
+  return [v];
+}
+
 function simpleCompare(a, b) {
   if (a === b) {
     return 0;
   }
 
-  const partsA = a.split("-");
-  const partsB = b.split("-");
+  const partsA = split(a, "-");
+  const partsB = split(b, "-");
 
   const splitsA = partsA[0].split(".");
   const splitsB = partsB[0].split(".");
+
   let i;
 
+  // do numerical compare on each part [major.minor.patch]
   for (i = 0; i < splitsA.length; i++) {
     const aN = parseInt(splitsA[i], 10);
     const bN = parseInt(splitsB[i], 10);
+
     if (aN > bN) {
       return -1;
     }
@@ -46,12 +58,9 @@ function simpleCompare(a, b) {
   } else if (partsB[1]) {
     // only B has parts after -
     return 1;
+  } else {
+    return 0; // numerical compare was the same
   }
-
-  //
-  // What could make the flow end up here?
-  //
-  throw new Error(`Malformed sem versions to compare ${a} and ${b}`);
 }
 
 //
@@ -70,21 +79,48 @@ function clean(v) {
   return f.join("-");
 }
 
-function fynify(v) {
-  const x = v.indexOf("-fynlocal");
+const FYN_LOCAL_TAG = "-fynlocal";
+
+function isLocal(v) {
+  return v.indexOf(FYN_LOCAL_TAG) > 0;
+}
+
+function localify(v, hash = "") {
+  return `${v}${FYN_LOCAL_TAG}${hash}`;
+}
+
+function unlocalify(v) {
+  const x = v.indexOf(FYN_LOCAL_TAG);
   return x > 0 ? v.substr(0, x) : v;
+}
+
+function localSplit(v) {
+  const x = v.indexOf(FYN_LOCAL_TAG);
+  if (x > 0) {
+    return [v.substr(0, x), v.substr(x)];
+  }
+
+  return [v];
 }
 
 module.exports = {
   satisfies: (v, semver) => {
-    return Semver.satisfies(fynify(v), semver);
+    return Semver.satisfies(unlocalify(v), semver);
   },
 
-  fynify,
+  split,
+
+  localify,
+  unlocalify,
+  isLocal,
 
   clean,
 
-  equal: (v1, v2) => fynify(v1) === fynify(v2),
+  equal: (v1, v2) => {
+    v1 = localSplit(v1);
+    v2 = localSplit(v2);
+    return v1[0] === v2[0] && (v1[1] && v2[1] ? v1[1] === v2[1] : true);
+  },
 
   simpleCompare
 };
---
+++ packages/fyn/test/spec/util/semver.spec.js
@@ -0,0 +1,120 @@
+"use strict";
+
+const semver = require("../../../lib/util/semver");
+
+describe("semver", function() {
+  describe("split", function() {
+    it("should split", () => {
+      expect(semver.split("0.8.3")).to.deep.equal(["0.8.3"]);
+      expect(semver.split("0.8.3", "-")).to.deep.equal(["0.8.3"]);
+      expect(semver.split("0.8.3", "-", 1)).to.deep.equal(["0.8.3"]);
+      expect(semver.split("0.8.3--rc")).to.deep.equal(["0.8.3--rc"]);
+      expect(semver.split("0.8.3--rc", "-")).to.deep.equal(["0.8.3", "-rc"]);
+
+      expect(semver.split("@foo/bar", "@", 1)).to.deep.equal(["@foo/bar"]);
+      expect(semver.split("@foo/bar", "@")).to.deep.equal(["", "foo/bar"]);
+      expect(semver.split("@foo/bar@0.8.3", "@", 1)).to.deep.equal(["@foo/bar", "0.8.3"]);
+    });
+  });
+
+  describe("simpleCompare", function() {
+    it("should handle 0.8.3 vs 0.8.3--rc", () => {
+      expect(semver.simpleCompare("0.8.3", "0.8.3--rc")).to.equal(1);
+      expect(semver.simpleCompare("0.8.3--rc", "0.8.3")).to.equal(-1);
+    });
+
+    it("should handle both have suffix", () => {
+      expect(semver.simpleCompare("0.8.3-a", "0.8.3-b")).to.equal(1);
+      expect(semver.simpleCompare("0.8.3-b", "0.8.3-a")).to.equal(-1);
+    });
+
+    it("should handle identical/same versions", () => {
+      expect(semver.simpleCompare("0.8.3", "0.8.3")).to.equal(0);
+      expect(semver.simpleCompare("10.08.003", "010.8.03")).to.equal(0);
+    });
+
+    it("should handle numerical diff versions", () => {
+      expect(semver.simpleCompare("0.8.4", "0.8.3")).to.equal(-1);
+      expect(semver.simpleCompare("0.09.3", "0.8.3")).to.equal(-1);
+      expect(semver.simpleCompare("01.8.3", "0.8.3")).to.equal(-1);
+
+      expect(semver.simpleCompare("0.8.3", "0.8.4")).to.equal(1);
+      expect(semver.simpleCompare("0.8.3", "0.09.3")).to.equal(1);
+      expect(semver.simpleCompare("0.8.3", "01.8.3")).to.equal(1);
+    });
+  });
+
+  describe("localify", function() {
+    it("should add tag", () => {
+      const x = semver.localify("0.1.1");
+      expect(x).to.equal("0.1.1-fynlocal");
+      expect(semver.isLocal(x)).to.be.true;
+    });
+
+    it("should add tag with hash", () => {
+      const x = semver.localify("0.1.1", "xyz");
+      expect(x).to.equal("0.1.1-fynlocalxyz");
+      expect(semver.isLocal(x)).to.be.true;
+    });
+  });
+
+  describe("unlocalify", function() {
+    it("should remove tag", () => {
+      expect(semver.unlocalify("0.1.1-fynlocal")).to.equal("0.1.1");
+    });
+
+    it("should remove tag with hash", () => {
+      expect(semver.unlocalify("0.1.1-fynlocalxyz")).to.equal("0.1.1");
+    });
+
+    it("should do nothing if there's no tag", () => {
+      expect(semver.unlocalify("0.1.1")).to.equal("0.1.1");
+    });
+  });
+
+  describe("equal", function() {
+    it("should return true for one fynlocal versions", () => {
+      expect(semver.equal("0.1.1-fynlocal", "0.1.1")).to.be.true;
+    });
+
+    it("should return true for two fynlocal versions", () => {
+      expect(semver.equal("0.1.1-fynlocal", "0.1.1-fynlocal")).to.be.true;
+    });
+
+    it("should return false for two fynlocal diff versions", () => {
+      expect(semver.equal("0.1.1-fynlocal", "0.1.2-fynlocal")).to.be.false;
+    });
+
+    it("should return false for two versions with diff fynlocals", () => {
+      expect(semver.equal("0.1.1-fynlocalxyz", "0.1.1-fynlocalabc")).to.be.false;
+    });
+  });
+
+  describe("satisfies", function() {
+    it("should return true for non fynlocal version", () => {
+      expect(semver.satisfies("0.1.1", "^0.1.0")).to.be.true;
+    });
+
+    it("should return true for fynlocal version", () => {
+      expect(semver.satisfies("0.1.1-fynlocal123", "^0.1.0")).to.be.true;
+    });
+
+    it("should return false for unmatch fynlocal version", () => {
+      expect(semver.satisfies("0.1.1-fynlocal123", "^0.2.0")).to.be.false;
+    });
+
+    it("should return false for unmatch  version", () => {
+      expect(semver.satisfies("0.1.1", "^0.2.0")).to.be.false;
+    });
+  });
+
+  describe("clean", function() {
+    it("should clean 3001.0001.0000-dev-harmony-fb", () => {
+      expect(semver.clean("3001.0001.0000-dev-harmony-fb")).to.equal("3001.1.0-dev-harmony-fb");
+    });
+
+    it("should clean 2.1.17+deprecated", () => {
+      expect(semver.clean("2.1.17+deprecated")).to.equal("2.1.17");
+    });
+  });
+});
