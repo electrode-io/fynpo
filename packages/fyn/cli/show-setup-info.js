ea60556fd36c4babe5cb5759de0aaa9b406c50a1
Joel Chen <joel123@gmail.com>
2018-01-26 15:56:05 -0800

    minor update messages

--- packages/fyn/cli/show-setup-info.js
+++ packages/fyn/cli/show-setup-info.js
@@ -7,9 +7,9 @@ const findFlatModule = require("./find-flat-module");
 module.exports = function() {
   if (process.platform === "win32") {
     const fmModule = findFlatModule();
-    logger.fyi("To setup for windows, run the command below:");
+    logger.fyi("To setup for Windows, run the command below:");
     logger.prefix(false).fyi(chalk.magenta(`set NODE_OPTIONS=-r ${fmModule}`));
-    logger.fyi(`You can the command "fyn win" to generate a file "fynwin.cmd" at your CWD.`);
+    logger.fyi(`You can use the command "fyn win" to generate a file "fynwin.cmd" at your CWD.`);
   } else {
     const setupSh = "eval `fyn bash`";
     logger.fyi(`To setup for bash, run the command "${chalk.cyan(setupSh)}"`);
