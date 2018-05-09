e51fd386cdf024ee15f6f157b5efd734029fe689
Joel Chen <joel123@gmail.com>
2018-05-09 02:13:58 -0700

    prefetch meta with a net queue

--- packages/fyn/lib/long-pending.js
+++ packages/fyn/lib/long-pending.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/* eslint-disable no-magic-numbers */
+/* eslint-disable no-magic-numbers, max-statements */
 
 const MAX_PENDING_SHOW = 10;
 const chalk = require("chalk");
@@ -12,11 +12,21 @@ module.exports = {
   onWatch: (items, options) => {
     options = options || {};
     const logItemName = options.name || LONG_WAIT_PACKAGE;
+
     if (items.total === 0) {
       logger.removeItem(logItemName);
       return;
     }
-    const all = items.watched.concat(items.still);
+
+    let all = items.watched.concat(items.still);
+
+    if (options.filter) all = all.filter(options.filter);
+
+    if (all.length === 0) {
+      logger.removeItem(logItemName);
+      return;
+    }
+
     if (!logger.hasItem(logItemName)) {
       logger.addItem({
         name: logItemName,
@@ -25,19 +35,21 @@ module.exports = {
       });
     }
     let msg = "";
-    if (items.total > MAX_PENDING_SHOW) {
-      msg = chalk.cyan(`Total: ${items.total}, first ${MAX_PENDING_SHOW}: `);
+    if (all.length > MAX_PENDING_SHOW) {
+      msg = chalk.cyan(`Total: ${all.length}, first ${MAX_PENDING_SHOW}: `);
     }
-    logger.updateItem(
-      logItemName,
-      msg +
-        all
-          .slice(0, MAX_PENDING_SHOW) // show max 10 pendings
-          .map(x => {
-            const id = typeof x.item === "string" ? chalk.magenta(x.item) : options.makeId(x.item);
-            return `${id} (${logFormat.timeWarn(x.time)})`;
-          })
-          .join(chalk.blue(", "))
-    );
+
+    msg += all
+      .slice(0, MAX_PENDING_SHOW) // show max 10 pendings
+      .map(x => {
+        const id = typeof x.item === "string" ? chalk.magenta(x.item) : options.makeId(x.item);
+        return `${id} (${logFormat.timeWarn(x.time)})`;
+      })
+      .join(chalk.blue(", "));
+
+    logger.updateItem(logItemName, {
+      msg,
+      _save: options._save !== undefined ? options._save : false
+    });
   }
 };
