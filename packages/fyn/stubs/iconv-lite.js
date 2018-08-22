85375699c21f86ec1a1c4d74a3f7d9d98ea711b0
Joel Chen <joel123@gmail.com>
2018-08-21 22:51:32 -0700

    update iconv stubs

--- packages/fyn/stubs/iconv-lite.js
+++ packages/fyn/stubs/iconv-lite.js
@@ -1,1 +1,7 @@
-throw new Error("iconv-lite module not bundled");
+const poof = () => {
+  throw new Error("iconv-lite is not bundled");
+};
+module.exports = {
+  encode: poof,
+  decode: poof
+};
--- packages/fyn/webpack.config.js
+++ packages/fyn/webpack.config.js
@@ -14,14 +14,14 @@ const base = {
       banner: "#!/usr/bin/env node",
       raw: true
     }),
-    process.env.ANALYZE_BUNDLE && new BundleAnalyzerPlugin(),
-    new webpack.IgnorePlugin(/\/iconv-loader$/)
+    process.env.ANALYZE_BUNDLE && new BundleAnalyzerPlugin()
   ].filter(x => x),
   resolve: {
     symlinks: false, // don't resolve symlinks to their real path
     alias: {
       xml2js: Path.resolve("stubs/xml2js.js"),
       "iconv-lite": Path.resolve("stubs/iconv-lite.js"),
+      "./iconv-loader": Path.resolve("stubs/iconv-loader.js"),
       debug: Path.resolve("stubs/debug.js"),
       lodash: require.resolve("lodash/lodash.min.js")
     }
