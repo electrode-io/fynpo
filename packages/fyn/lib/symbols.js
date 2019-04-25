810d240ec43221da66d09c805331b65f922fab9c
Joel Chen <joel123@gmail.com>
2019-04-24 23:30:29 -0700

    resolve with respect to latest tag

--- packages/fyn/lib/symbols.js
+++ packages/fyn/lib/symbols.js
@@ -4,7 +4,10 @@ const LOCK_RSEMVERS = Symbol("lock rsemvers"); // resolved semvers from lock
 const SEMVER = Symbol("semver"); // depInfo semver
 const RSEMVERS = Symbol("rsemvers"); // resolved semvers
 const LOCK_SORTED_VERSIONS = Symbol("lock sorted versions");
+const LATEST_TAG_VERSION = Symbol("latest tag version");
+const LATEST_VERSION_TIME = Symbol("latest version time");
 const SORTED_VERSIONS = Symbol("sorted versions");
+const LATEST_SORTED_VERSIONS = Symbol("latest sorted versions");
 const LOCAL_VERSION_MAPS = Symbol("local version maps");
 const RESOLVE_ORDER = Symbol("resolve order");
 const PACKAGE_RAW_INFO = Symbol("package.json raw info");
@@ -15,7 +18,10 @@ module.exports = {
   RSEMVERS,
   LOCK_RSEMVERS,
   SORTED_VERSIONS,
+  LATEST_VERSION_TIME,
+  LATEST_SORTED_VERSIONS,
   LOCK_SORTED_VERSIONS,
+  LATEST_TAG_VERSION,
   LOCAL_VERSION_MAPS,
   RESOLVE_ORDER,
   PACKAGE_RAW_INFO,
