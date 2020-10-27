6f7ae7731fe670f96ee1a0d440d183d31cefc1e1
Joel Chen <xchen@walmartlabs.com>
2020-10-27 00:41:53 -0700

    use xaa.makeDefer

--- packages/fyn/lib/util/defer.js
+++ packages/fyn/lib/util/defer.js
@@ -1,14 +1,3 @@
 "use strict";
 
-const Promise = require("bluebird");
-
-module.exports = function _defer() {
-  const defer = {};
-
-  defer.promise = new Promise((resolve, reject) => {
-    defer.resolve = resolve;
-    defer.reject = reject;
-  });
-
-  return defer;
-};
+module.exports = require("xaa").makeDefer;
