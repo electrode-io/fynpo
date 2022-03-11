0a18dce5979d9fa2097fccdb119da5b75b7950aa
Joel Chen <joel123@gmail.com>
2022-03-11 09:56:47 -0800

    pkg install check buildLocal flag

--- packages/fyn/lib/pkg-installer.js
+++ packages/fyn/lib/pkg-installer.js
@@ -429,10 +429,8 @@ class PkgInstaller {
   }
 
   async _buildLocalPkg(depInfo) {
-    if (this._fyn._localPkgBuilder) {
+    if (this._fyn._options.buildLocal && this._fyn._localPkgBuilder) {
       await this._fyn._localPkgBuilder.waitForItem(depInfo.dir);
-    } else {
-      logger.error("pkg-installer: there is no this._fyn._localPkgBuilder");
     }
   }
 
